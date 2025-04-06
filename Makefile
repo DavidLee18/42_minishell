d7551e3c5dd5315cbb74a589b94fa1b8d6fb5ccd
Jaehyun Lee <jaehylee@c3r11s1.42gyeongsan.kr>
2025-04-07 05:03:58 +0900

    define proto-types

---
+++ Makefile
@@ -0,0 +1,54 @@
+CC := cc
+CFLAGS := -Wall -Werror -Wextra -O2 -foptimize-sibling-calls
+NAME := minishell
+BFLAGS := -Lft_printf -lftprintf
+# BONUS_NAME := pipex_bonus
+
+BUILD_DIR := build
+SRC_DIR := src
+
+SRCS :=
+# BONUS_SRCS := src/main_bonus.c src/fd_cleaner.c \
+#			src/path.c src/files_pipes_bonus.c \
+#			src/getln_until_bonus.c
+
+OBJS := $(patsubst src/%.c, build/%.o, $(SRCS))
+# BONUS_OBJS := $(patsubst src/%.c,build/%.o,$(BONUS_SRCS))
+
+
+all: $(NAME)
+
+# bonus: $(BONUS_NAME)
+
+$(NAME): $(OBJS)
+	make -C ft_printf
+	$(CC) $(OBJS) $(BFLAGS) -o $(NAME)
+
+build/%.o: src/%.c
+	mkdir -p $(BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(SRCS): $(SRC_DIR)/minishell.h
+
+clean:
+	rm -rf $(BUILD_DIR)
+	make clean -C ft_printf
+
+fclean:	clean
+	rm -f $(NAME)
+#	rm -f $(BONUS_NAME)
+	make fclean -C ft_printf
+
+# $(BONUS_NAME): $(BONUS_OBJS)
+#	make -C ft_printf
+#	$(CC) $(BONUS_OBJS) $(BFLAGS) -o $(BONUS_NAME)
+
+
+# $(BONUS_OBJS): $(BONUS_SRCS)
+#	$(CC) $(CFLAGS) -c $(patsubst build/%.o,src/%.c,$@)
+#	mkdir -p build
+#	mv $(patsubst build/%.o,%.o,$@) $@
+
+re:	fclean all
+
+.PNONY: all clean fclean re
---
+++ src/minishell.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 23:03:52 by jaehylee          #+#    #+#             */
+/*   Updated: 2025/04/07 04:49:27 by jaehylee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <curses.h>
+# include <dirent.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <stdio.h>
+# include <string.h>
+# include <sysexits.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <term.h>
+# include <termios.h>
+# include "../ft_printf/src/ft_printf.h"
+
+typedef enum e_cmd_type
+{
+	BUILTIN,
+	NORMAL,
+	REDIR_IN,
+	REDIR_OUT,
+	HERE_DOC,
+	REDIR_APND
+}	t_cmd_type;
+
+typedef union u_debris
+{
+	int		fd;
+	char	*delim;
+	char	**argv;
+}	t_debris;
+
+typedef struct s_phrase
+{
+	t_cmd_type		type;
+	t_debris		deb;
+	struct s_phrase	*pred;
+	struct s_phrase	*succ;
+}	t_phrase;
+
+char		**lex(t_list **dyn, const char *str);
+t_phrase	*parse(t_list **dyn, const char **tokens);
+
+#endif
