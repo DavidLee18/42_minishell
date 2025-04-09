9feefb5c07ea24d971fd9ad6eb1bbd3a87dd85ef
Jaehyun Lee <jaehylee@c3r11s5.42gyeongsan.kr>
2025-04-09 17:50:54 +0900

    impl lexer

--- src/lex2.c
+++ src/lex2.c
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:52:01 by jaehylee          #+#    #+#             */
-/*   Updated: 2025/04/09 10:58:28 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:47:15 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ _Bool	lex_split(t_list **dyn, char **split, char const *s)
 	i = 0;
 	q = NONE;
 	sp[1] = (t_split_piece){.start = -1, .length = -1};
+	sp[2] = (t_split_piece){.start = 0, .length = 0};
 	while (s[i])
 	{
 		next = lex_split_pos(s, i, sp, &q);
@@ -29,16 +30,16 @@ _Bool	lex_split(t_list **dyn, char **split, char const *s)
 		{
 			if (!lex_split_range(dyn, split, s, sp))
 				return (0);
-			sp[1].start = next;
+			sp[2].start = next;
 			i = next;
 		}
 		else
 			i++;
 	}
-	return (1);
+	return (lex_split_final(dyn, split, s, sp));
 }
 
-ssize_t	lex_split_pos(const char *s, size_t i, t_split_piece *sp, t_quote *q)
+size_t	lex_split_pos(const char *s, size_t i, t_split_piece *sp, t_quote *q)
 {
 	if ((s[i] == '\'' && *q != DOUBLE) || (s[i] == '\"' && *q != SINGLE))
 	{
@@ -46,37 +47,49 @@ ssize_t	lex_split_pos(const char *s, size_t i, t_split_piece *sp, t_quote *q)
 			*q = SINGLE - *q;
 		else
 			*q = DOUBLE - *q;
-		sp->start = sp[1].start;
-		sp->length = i - sp[1].start + (*q == NONE);
+		sp->start = sp[2].start;
+		sp->length = i - sp[2].start + (*q == NONE);
 		return (sp->start + sp->length);
 	}
 	else
-		return (lex_split_pos2(s, i, sp));
+		return (lex_split_pos2(s, i, sp, q));
 }
 
-ssize_t	lex_split_pos2(const char *s, size_t i, t_split_piece *sp)
+size_t	lex_split_pos2(const char *s, size_t i, t_split_piece *sp, t_quote *q)
 {
-	if (ft_strncmp(s + i, ">>", 2) == 0 || ft_strncmp(s + i, "<<", 2))
+	if (*q != NONE)
+		return (0);
+	if (ft_strncmp(s + i, ">>", 2) == 0 || ft_strncmp(s + i, "<<", 2) == 0)
 	{
-		sp->start = sp[1].start;
-		sp->length = i - sp[1].start;
+		sp->start = sp[2].start;
+		sp->length = i - sp[2].start;
 		sp[1].start = i;
 		sp[1].length = 2;
 		return (i + 2);
 	}
 	else if (s[i] == '<' || s[i] == '|' || s[i] == '>')
 	{
-		sp->start = sp[1].start;
-		sp->length = i - sp[1].start;
+		sp->start = sp[2].start;
+		sp->length = i - sp[2].start;
 		sp[1].start = i;
 		sp[1].length = 1;
 		return (i + 1);
 	}
 	else if (is_space(s[i]))
 	{
-		sp->start = sp[1].start;
-		sp->length = i - sp[1].start;
+		sp->start = sp[2].start;
+		sp->length = i - sp[2].start;
 		return (i + 1);
 	}
-	return (-1);
+	return (0);
+}
+
+_Bool	lex_split_final(t_list **dyn, char **split, char const *s,
+			t_split_piece *sp)
+{
+	sp->start = sp[2].start;
+	sp->length = ft_strlen(s) - sp[2].start;
+	if (!lex_split_range(dyn, split, s, sp))
+		return (0);
+	return (1);
 }
--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 23:03:52 by jaehylee          #+#    #+#             */
-/*   Updated: 2025/04/09 10:22:34 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:43:34 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,9 +76,12 @@ void		lex_pre_quote(char c, t_split_next *idxs, t_quote *p);
 _Bool		lex_split(t_list **dyn, char **split, char const *s);
 _Bool		lex_split_range(t_list **dyn, char **split, char const *s,
 				t_split_piece *sp);
-ssize_t		lex_split_pos(const char *s, size_t i, t_split_piece *sp,
+size_t		lex_split_pos(const char *s, size_t i, t_split_piece *sp,
 				t_quote *p);
-ssize_t		lex_split_pos2(const char *s, size_t i, t_split_piece *sp);
+size_t		lex_split_pos2(const char *s, size_t i, t_split_piece *sp,
+				t_quote *q);
+_Bool		lex_split_final(t_list **dyn, char **split, char const *s,
+				t_split_piece *sp);
 t_phrase	*parse(t_list **dyn, const char **tokens);
 _Bool		is_space(char c);
 
