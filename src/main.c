5c8cd463ee9873550c386598339b131b098ed6bb
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2025-04-12 22:06:43 +0900

    no need to set sa_handlers again

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:01:59 by jaehylee          #+#    #+#             */
-/*   Updated: 2025/04/12 21:55:17 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/04/12 22:04:20 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ int	main(void)
 
 	dyn = NULL;
 	g_signal = 0;
+	if (!handle_signals())
+		return (gc_free_all(dyn), 1);
 	while (1)
 	{
-		if (!handle_signals())
-			return (gc_free_all(dyn), 1);
 		str = prompt(&dyn);
 		if (!str)
 			return (ft_fprintf(STDOUT_FILENO, "exit\n"), gc_free_all(dyn), 0);
